1ce058dd97fbea75c4cdd91abccbc755ef71a78c
kjbin0420 <kjbin0420@naver.com>
2021-01-18 22:14:21 +0900

    [ADD] Exception Handler 추가, 기본적인 로깅 처리 추가

---
+++ src/main/java/com/dms/pms/error/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.dms.pms.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+}
