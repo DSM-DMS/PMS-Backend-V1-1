67067ba7ab47a8297d8976031dc4383008f300d7
smoothbear <kjbin0420@naver.com>
2021-01-20 16:25:11 +0900

    [ADD] Multiple DataSource 사용을 위한 Datasource, Transaction 관련 Config 추가

---
+++ src/main/java/com/dms/pms/config/PmsDbConfig.java
@@ -0,0 +1,53 @@
+package com.dms.pms.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "entityManagerFactory",
+        basePackages = { "com.dms.pms.entity.pms"}
+)
+public class PmsDbConfig {
+    @Primary
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("dataSource") DataSource dataSource
+    ){
+        return builder
+                .dataSource(dataSource)
+                .packages("com.dms.pms.entity.pms")
+                .persistenceUnit("pms")
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory
+            ){
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
