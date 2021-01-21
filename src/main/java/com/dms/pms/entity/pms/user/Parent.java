7f555d77e401293b9582b71507d37828bd81ede6
smoothbear <kjbin0420@naver.com>
2021-01-21 16:55:29 +0900

    [ADD] 유저 권한 추가에 따른 회원가입 로직 수정

--- src/main/java/com/dms/pms/entity/pms/user/Parent.java
+++ src/main/java/com/dms/pms/entity/pms/user/Parent.java
@@ -1,8 +1,10 @@
 package com.dms.pms.entity.pms.user;
 
 import com.dms.pms.entity.pms.student_user.StudentUser;
+import com.dms.pms.security.auth.RoleType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -11,7 +13,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "parent")
-@AllArgsConstructor @NoArgsConstructor @Builder
+@AllArgsConstructor @NoArgsConstructor @Builder @Getter
 public class Parent {
     @Id
     private String email;
@@ -21,6 +23,10 @@ public class Parent {
 
     @Column(nullable = false)
     private String name;
+
+    @Column(name = "user_role")
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
     @Builder.Default
     @OneToMany(mappedBy = "parent")
---
+++ src/main/java/com/dms/pms/security/auth/RoleType.java
@@ -0,0 +1,5 @@
+package com.dms.pms.security.auth;
+
+public enum RoleType {
+    ADMIN, USER
+}
