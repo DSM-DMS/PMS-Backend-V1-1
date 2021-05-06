80587fe7d931071d6ce746d72e3c1c8d633d1ee9
smoothbear <kjbin0420@naver.com>
2021-05-06 09:18:30 +0900

    [ADD] FCM 메세지 로직 추가

--- src/main/java/com/dms/pms/service/student/StudentServiceImpl.java
+++ src/main/java/com/dms/pms/service/student/StudentServiceImpl.java
@@ -2,17 +2,29 @@ package com.dms.pms.service.student;
 
 import com.dms.pms.entity.pms.outing.Outing;
 import com.dms.pms.entity.pms.outing.OutingRepository;
+import com.dms.pms.entity.pms.student.Student;
+import com.dms.pms.entity.pms.student.StudentRepository;
+import com.dms.pms.entity.pms.user.User;
+import com.dms.pms.exception.StudentNotFoundException;
 import com.dms.pms.payload.request.AddOutingRequest;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.WebpushConfig;
+import com.google.firebase.messaging.WebpushNotification;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Set;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
 
     private final OutingRepository outingRepository;
+    private final StudentRepository studentRepository;
 
     @Override
     public void addOuting(AddOutingRequest request) {
@@ -25,5 +37,24 @@ public class StudentServiceImpl implements StudentService {
                 .type(request.getType())
                 .build()
         );
+
+        Student student = studentRepository.findByStudentNumber(request.getNumber())
+                .orElseThrow(StudentNotFoundException::new);
+
+        Set<User> users = student.getUsers();
+
+        users.forEach(receiver -> {
+                    Message message = Message.builder()
+                            .setToken("l")
+                            .setWebpushConfig(WebpushConfig.builder().putHeader("ttl", "300")
+                                    .setNotification(
+                                            new WebpushNotification(student.getName() + "님이 외출하였습니다.",
+                                                    "장소: " + request.getPlace()))
+                                    .build())
+                            .build();
+
+                    String response = FirebaseMessaging.getInstance().sendAsync(message).get();
+                    log.info("Sent message: {}", response);
+                });
     }
 }
